4effca76563a0b92c65e46345daab3a5da358dce
Severin Nandillon <nandillons@gmail.com>
2020-11-14 16:28:26 +0100

    Exercice gestionnaire de téléchargements

---
+++ gestionnaire de téléchargement/gestionnaire de téléchargement/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace gestionnaire_de_téléchargement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entrez des url séparés par des espaces. Quand vous avez fini, appuyez sur \"Entrer\" pour lancer les téléchargements.\n");
+
+            List<string> listeUrls = new List<string>();
+            string urls = Console.ReadLine();
+            string[] tabUrls = urls.Split(' ');
+            Regex rgx = new Regex(@"/((http|https)://)*[a-zA-Z0-9\.\-_]+\.[a-z0-9]+/([a-zA-Z0-9\.\-_]+/)*[a-zA-Z0-9\.\-_]+\.[a-z0-9]+(\?[a-zA-Z0-9\.\-_=&:]*)*");
+
+            foreach (string url in tabUrls)
+            {
+                if (rgx.IsMatch(url)) {
+                    listeUrls.Add(url);
+                }
+                else
+                {
+                    Console.WriteLine($"L'url {url} n'est pas valide, le téléchargement de ce fichier est impossible.");
+                }
+            }
+
+            var t = new Telechargement();
+            Console.WriteLine("\nTéléchargement asynchrone de fichiers :");
+            var taches = listeUrls.Select(url => t.TelechargementFichier(url)).ToArray();
+            Task.WaitAll(taches);
+        }
+    }
+}
---
+++ gestionnaire de téléchargement/gestionnaire de téléchargement/Telechargement.cs
@@ -0,0 +1,28 @@
+﻿using System.Net;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System;
+
+namespace gestionnaire_de_téléchargement
+{
+    class Telechargement
+    {
+        public async Task TelechargementFichier(string url)
+        {
+            using (var wc = new WebClient())
+            {
+                // récupérer le nom du fichier dans l'url
+                string pattern = @"[a-zA-Z0-9\.\-_]+\.[a-z0-9]+";
+                var nomFichier = url.Substring(url.LastIndexOf('/') + 1);
+                MatchCollection mc = Regex.Matches(nomFichier, pattern);
+                nomFichier = mc[0].ToString();
+                string dossierTelechargement = @"C:\Users\nandi\Downloads\" + nomFichier;
+                //
+
+                Console.WriteLine($"    Téléchargement de {nomFichier} commencé.");
+                await wc.DownloadFileTaskAsync(url, dossierTelechargement);
+                Console.WriteLine($"    Téléchargement de {nomFichier} fini.");
+            }
+        }
+    }
+}
