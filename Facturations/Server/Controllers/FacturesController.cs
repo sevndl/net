57e27433b851675080b4da6b52e5943ddc3db11b
Severin Nandillon <nandillons@gmail.com>
2020-12-10 14:38:33 +0100

    API -> liste des factures

---
+++ Facturations/Server/Controllers/FacturesController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Facturations.Shared;
+
+namespace Facturations.Server.Controllers
+{
+    [ApiController]
+    [Route("/api/[controller]")]
+    public class FacturesController : ControllerBase
+    {
+
+        private readonly ILogger<FacturesController> _logger;
+        private readonly BusinessData _data;
+
+        public FacturesController(ILogger<FacturesController> logger, BusinessData data)
+        {
+            _logger = logger;
+            _data = data;
+        }
+
+        [HttpGet]
+        public IEnumerable<Facture> Get()
+        {
+            return _data.Factures;
+        }
+    }
+}
